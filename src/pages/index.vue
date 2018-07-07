8aaaebbf40e88372d7a2b5e0610f32c87ef376d4
Peter Schrammel <peter.schrammel@gmx.de>
2018-07-07 21:38:20 +0000

    adding appolo

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <q-page class="flex flex-center">
     <q-btn @click="login" >Login</q-btn>
+    ({{account.email}})
+    <div v-if="$apollo.loading">Loading...</div>
   </q-page>
 </template>
 
@@ -8,13 +10,53 @@
 </style>
 
 <script>
+import gql from 'graphql-tag'
+import Config from '../lib/config'
+const config = new Config({ok: "yes"})
+
 export default {
   name: 'PageIndex',
-  
+  data() {
+    return {
+      account: {}
+    }
+  },
+  apollo: {
+    xaccount: {
+      query: gql`{
+               account(id: 2){
+                 email
+                 id
+               }
+             }`,
+             
+      variables: {
+        accounId: 2,
+      },   
+    }
+  },
   methods: {
     login() {
-      console.log("Login")
-    }
+      console.log("Login start")
+      var client = this.$apollo.provider.defaultClient
+      client.query(
+      {
+        query: gql`{
+                account(id: 2){
+                            email
+                            id
+                            }
+                  }`,
+             
+       variables: {
+        accounId: 2,
+      },   
+    }).then((result) => {
+      this.account=result.data.account
+      console.log("Login", client, result.data.account)
+      console.log(config.get('ok'))
+    })
+   }
   }
 }
 </script>
